1c2a790ed121515ccde8d67c81b0f18483fc890b
infan11 <infanjiounrahman20606@gmail.com>
2025-08-08 21:25:01 +0600

    some bug and error solve

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,16 +1,24 @@
 import mongoose from 'mongoose';
-const MONGODB_URL = process.env.MONGODB_URL
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
 if (!MONGODB_URL) {
-    throw new Error("please difine mongo environment variable")
+    throw new Error("Please define MONGODB_URL in your environment variables");
 }
+
 async function connectToDatabase() {
     if (mongoose.connection.readyState === 1) {
         return mongoose;
     }
     const opts = {
         bufferCommands: false,
+    };
+    if (!MONGODB_URL) {
+        throw new Error("Please define the MONGODB_URL environment variable");
     }
-    await mongoose.connect(MONGODB_URL!, opts);
-    return mongoose
+    await mongoose.connect(MONGODB_URL, opts);
+
+    return mongoose;
 }
+
 export default connectToDatabase;
