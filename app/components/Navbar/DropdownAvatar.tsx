4d557ee99d0dc44ddc35f7fe37eae1812c2d4dd5
infan11 <infanjiounrahman20606@gmail.com>
2025-08-11 16:47:28 +0600

    AvatarFallback image add in DropdownAvatar

--- app/components/Navbar/DropdownAvatar.tsx
+++ app/components/Navbar/DropdownAvatar.tsx
@@ -7,19 +7,21 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
-  
+
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { signOut, useSession } from "next-auth/react"
 import { LogOut } from "lucide-react";
+import Image from "next/image";
+import profileImage from './../../../public/image/businessman-character-avatar-isolated.png'
 export function DropdownAvatar() {
   const { data: session } = useSession();
   return (
     <div className="">
       <DropdownMenu >
         <DropdownMenuTrigger asChild>
-        {/* <motion.button
+          {/* <motion.button
             whileHover={{ scale: 1.0, rotate: 5 }}
             whileTap={{ scale: 0.95 }}
             className="btn h-10 px-4 rounded-full bg-white text-black border border-gray-300 hover:text-green-500 transition"
@@ -28,7 +30,7 @@ export function DropdownAvatar() {
           </motion.button> */}
           <Avatar>
             <AvatarImage className="w-10 h-10  rounded-full border-2 border-green-100" src={session?.user?.image ?? ""} />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarFallback><Image src={profileImage} alt="unavilable" width={40} height={40} className="rounded-full border-2" /></AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56 mr-4.5" align="start">
@@ -38,19 +40,19 @@ export function DropdownAvatar() {
               Profile
               <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
             </DropdownMenuItem>
-          
+
             <DropdownMenuItem>
               Settings
               <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
             </DropdownMenuItem>
-      
+
           </DropdownMenuGroup>
-    
+
           <DropdownMenuItem>Support</DropdownMenuItem>
-   
+
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={() => signOut()}>
-           <LogOut/> Log out
+            <LogOut /> Log out
             {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
           </DropdownMenuItem>
         </DropdownMenuContent>
--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol : "https",
-        hostname :  'i.ibb.co',
-        port : "",
-        pathname : "/**"
+        protocol: "https",
+        hostname: 'i.ibb.co',
+        port: "",
+        pathname: "/**"
       }
     ]
   },
