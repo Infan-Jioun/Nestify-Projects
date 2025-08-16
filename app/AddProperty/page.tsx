d9085d30dc86b6480a7cf30e082996cb531fb31f
infan11 <infanjiounrahman20606@gmail.com>
2025-08-16 11:17:53 +0600

    added NextHead problem dixed

--- app/AddProperty/page.tsx
+++ app/AddProperty/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import NextHead from "../components/NextHead/NextHead";
-
-
-
-
-
 export default function AddPropertyPage() {
   return (
     <div>
       <div>
-        <NextHead title="Add Property - Nestify" />        
+        <NextHead title="Add Property - Nestify" />
       </div>
 
     </div>
