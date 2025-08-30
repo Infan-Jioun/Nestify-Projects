b7ae69f0fbd6f53f353ae06d65eff53a6c2ef9db
infan11 <infanjiounrahman20606@gmail.com>
2025-08-30 12:23:26 +0600

    update nav-user.tsx conponents

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -7,7 +7,6 @@ import {
   CreditCard,
   LogInIcon,
   LogOut,
-  Sparkles,
 } from "lucide-react"
 
 import {
@@ -31,10 +30,11 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 import Image from "next/image"
-import profileImage from './../public/image/businessman-character-avatar-isolated.png'
+import profileImage from "./../public/image/businessman-character-avatar-isolated.png"
 import { signOut, useSession } from "next-auth/react"
 import Link from "next/link"
 import { motion } from "framer-motion"
+
 export function NavUser({
   user,
 }: {
@@ -44,112 +44,116 @@ export function NavUser({
     avatar: string
   }
 }) {
-  const { data: session } = useSession();
+  const { data: session } = useSession()
   const { isMobile } = useSidebar()
 
   const handelSignOut = async () => {
     await signOut({ redirect: false })
   }
+
   return (
     <SidebarMenu>
-      {
-        session ? (<SidebarMenuItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton
-                size="lg"
-                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-              >
-                <Avatar>
-                  <AvatarImage className="w-10 h-10  rounded-full border-2 border-green-100" src={user?.avatar ?? ""} />
-                  <AvatarFallback><Image src={profileImage} alt="unavilable" width={40} height={40} className="rounded-full border-2" /></AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
-                </div>
-                <ChevronsUpDown className="ml-auto size-4" />
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-              side={isMobile ? "bottom" : "right"}
-              align="end"
-              sideOffset={4}
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                  <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={user?.avatar ?? ""} alt={user.name} />
-                    <AvatarFallback><Image src={profileImage} alt="unavilable" width={40} height={40} className="rounded-full border-2" /></AvatarFallback>
-                  </Avatar>
-                  <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-medium">{user.name}</span>
-                    <span className="truncate text-xs">{user.email}</span>
+              <Avatar>
+                <AvatarImage
+                  className="w-10 h-10 rounded-full border-2 border-green-100"
+                  src={user?.avatar ?? ""}
+                />
+                <AvatarFallback>
+                  <Image
+                    src={profileImage}
+                    alt="unavailable"
+                    width={40}
+                    height={40}
+                    className="rounded-full border-2"
+                  />
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">
+                  {session ? user.name : "Guest"}
+                </span>
+                <span className="truncate text-xs">
+                  {session ? user.email : "Not logged in"}
+                </span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            {session ? (
+              <>
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                    <Avatar className="h-8 w-8 rounded-lg">
+                      <AvatarImage src={user?.avatar ?? ""} alt={user.name} />
+                      <AvatarFallback>
+                        <Image
+                          src={profileImage}
+                          alt="unavailable"
+                          width={40}
+                          height={40}
+                          className="rounded-full border-2"
+                        />
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                      <span className="truncate font-medium">{user.name}</span>
+                      <span className="truncate text-xs">{user.email}</span>
+                    </div>
                   </div>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <Sparkles />
-                  Upgrade to Pro
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  <BadgeCheck />
-                  Account
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <CreditCard />
-                  Billing
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Bell />
-                  Notifications
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
-              {
-                session ? (<DropdownMenuItem onClick={() => handelSignOut()}>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <BadgeCheck />
+                    Account
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCard />
+                    Billing
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Bell />
+                    Notifications
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem onClick={handelSignOut}>
                   <LogOut />
                   Log out
-                </DropdownMenuItem>) : (
-                  <DropdownMenuItem>
-
-                    <Link href="/LoginPage">
-                      <motion.button
-                        whileHover={{ scale: 1.0 }}
-                        whileTap={{ scale: 0.95 }}
-                        className="w-full px-4 flex justify-center items-center p-2 gap-2 rounded-full hover:text-white bg-green-500 text-white hover:bg-green-600 font-semibold transition"
-                      >
-                        <LogInIcon /> Login
-                      </motion.button>
-                    </Link>
-                  </DropdownMenuItem>
-                )
-              }
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </SidebarMenuItem>) : (<DropdownMenu>
-          <DropdownMenuContent>
-            <DropdownMenuItem>
-
-              <Link href="/LoginPage">
-                <motion.button
-                  whileHover={{ scale: 1.0 }}
-                  whileTap={{ scale: 0.95 }}
-                  className="w-full px-4 flex justify-center items-center p-2 gap-2 rounded-full hover:text-white bg-green-500 text-white hover:bg-green-600 font-semibold transition"
-                >
-                  <LogInIcon /> Login
-                </motion.button>
-              </Link>
-            </DropdownMenuItem>
+                </DropdownMenuItem>
+              </>
+            ) : (
+              <DropdownMenuItem asChild>
+                <Link href="/LoginPage" className="w-full">
+                  <motion.button
+                    whileHover={{ scale: 1.0 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="w-full px-4 flex justify-center items-center p-2 gap-2 rounded-full bg-green-500 text-white hover:bg-green-500 hover:text-white  font-semibold transition"
+                  >
+                    <LogInIcon /> Login
+                  </motion.button>
+                </Link>
+              </DropdownMenuItem>
+            )}
           </DropdownMenuContent>
-        </DropdownMenu>)
-      }
+        </DropdownMenu>
+      </SidebarMenuItem>
     </SidebarMenu>
   )
 }
