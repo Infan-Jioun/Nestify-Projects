2d656183c237cc9d9afd1409f75b13c6efa1ac49
infan11 <infanjiounrahman20606@gmail.com>
2025-08-30 15:46:00 +0600

    properties SSR use Route.ts and data load propeties

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -19,3 +19,20 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ message: "Internal Server Error" }, { status: 500 })
     }
 }
+export async function GET(req: NextRequest) {
+    await connectToDatabase();
+    try {
+        const data = await req.json()
+        console.log(data);
+        const properties = await Property.find({});
+        if (!properties) {
+            return NextResponse.json(properties, { status: 200 });
+        } else {
+            return NextResponse.json(properties, { status: 404, statusText: "No users found" });
+        }
+    } catch (error) {
+        console.error("Error fetching properties:", error);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+
+    }
+}
