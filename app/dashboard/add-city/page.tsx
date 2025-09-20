ea0699dea73c2939031805cef4ff40552ed0dc7e
infan11 <infanjiounrahman20606@gmail.com>
2025-09-20 11:01:02 +0600

    added city Banner and AddCity Details

--- app/dashboard/add-city/page.tsx
+++ app/dashboard/add-city/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CityBanner from './components/CityBanner'
+import AddCityForm from './components/AddCityForm'
 
 export default function AddCity() {
     return (
         <div>
 
             <CityBanner />
+            <AddCityForm />
         </div>
     )
 }
