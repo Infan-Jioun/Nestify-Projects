9f84ae515221bd00fd639762ca9e0699a88bb75c
infan11 <infanjiounrahman20606@gmail.com>
2025-10-02 20:33:46 +0600

    feat: update search functionality with recent searches, suggestions, and not-found message

--- app/components/Navbar/Search/SearchBar.tsx
+++ app/components/Navbar/Search/SearchBar.tsx
@@ -211,7 +211,7 @@ const SearchBox: React.FC = () => {
                             {/* Not Found */}
                             {query && query.length >= 2 && suggestions.length === 0 && !loading && (
                                 <div className="p-4 text-center text-gray-500">
-                                        No results found for &quot;"{query}"
+                                        No results found for &quot;{query}&quot;
                                     </div>
                             )}
 
